f632fc7f33030dedfb900540f9c5755e044df218
Sebastian Villegas <svillegasz@Sebastian-Villegas-MacBook-Pro.local>
2025-02-05 22:09:57 -0500

    added retry mechanism to get gmail messages

--- gmail.py
+++ gmail.py
@@ -11,14 +11,25 @@ imap = imaplib.IMAP4_SSL(imap_host)
 imap.login(imap_user, imap_pass)
 imap.select('Inbox')
 
-def get_messages(sender):
+def get_messages(sender, retries=3):
     print('Gmail imap: getting messages for {sender}'.format(sender=sender))
-    imap.literal = f'from:({sender}) newer_than:1d in:inbox'.encode('utf-8')
-    response, data = imap.uid('SEARCH', 'CHARSET', 'UTF-8', 'X-GM-RAW')
-    if response != 'OK':
-        print('Gmail imap: No gmail messages found')
-        return
-    return data[0].split()
+    for attempt in range(retries):
+        try:
+            imap.literal = f'from:({sender}) newer_than:1d in:inbox'.encode('utf-8')
+            response, data = imap.uid('SEARCH', 'CHARSET', 'UTF-8', 'X-GM-RAW')
+            if response != 'OK':
+                print('Gmail imap: No gmail messages found')
+                return
+            return data[0].split()
+        except imaplib.IMAP4.abort as e:
+            print(f'Attempt {attempt + 1} failed: {e}')
+            if attempt < retries - 1:
+                print('Retrying...')
+                imap.logout()  # Logout to reset the connection
+                imap.login(imap_user, imap_pass)  # Re-login
+            else:
+                print('Max retries reached. Exiting.')
+                return
 
 def get_message(msg_id):
     print('Gmail imap: getting message with id {msg_id}'.format(msg_id=msg_id))
